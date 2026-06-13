9b44b38b33edd7c99442a30ffce8ed9465a1f443
Ronak Sharma <84510554+officialronaks@users.noreply.github.com>
2026-06-13 14:00:02 -0400

    Connected homepage to Firestore

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,34 @@
-console.log('CustomizeHub loaded');
+import {
+    db,
+    doc,
+    getDoc
+} from "../../firebase/firestore.js";
+
+async function loadSettings() {
+
+    const docRef = doc(db, "settings", "site");
+
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+
+        const data = docSnap.data();
+
+        document.getElementById("siteName").innerText =
+            data.siteName || "CustomizeHub";
+
+        document.getElementById("heroTitle").innerText =
+            data.heroTitle || "";
+
+        document.getElementById("heroSubtitle").innerText =
+            data.heroSubtitle || "";
+
+    } else {
+
+        console.log("settings/site not found");
+
+    }
+
+}
+
+loadSettings();
--- firebase/firestore.js
+++ firebase/firestore.js
@@ -1,5 +1,25 @@
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
 
-import { app } from "./config.js";
+import {
+    getFirestore,
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDfymITNIldZOlFTyXSWlJdI5tXS1-7hLg",
+    authDomain: "customizehub-prod.firebaseapp.com",
+    projectId: "customizehub-prod",
+    storageBucket: "customizehub-prod.firebasestorage.app",
+    messagingSenderId: "210026228677",
+    appId: "1:210026228677:web:bfc30418aff4a93848b9ac"
+};
+
+const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+
+export {
+    doc,
+    getDoc
+};
